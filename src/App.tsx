69c2d38b189876c19112915776c7d39a5fbb9d76
JulienLEUILLIER <julien.leuillier@gmail.com>
2022-02-28 17:47:41 +0100

    First routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import DesignSystem from './components/DesignSystem';
+import GlobalStyle from './global';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/design-system" element={<DesignSystem />} />
+        </Routes>
       </header>
     </div>
   );
