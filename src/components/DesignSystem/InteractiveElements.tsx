bb128b56a1fe9458b31f70e43f7dc0a9e2ae872e
JulienLEUILLIER <julien.leuillier@gmail.com>
2022-03-02 13:18:47 +0100

    navigation bar in design system

--- src/components/DesignSystem/InteractiveElements.tsx
+++ src/components/DesignSystem/InteractiveElements.tsx
@@ -4,6 +4,19 @@ const InteractiveElements = () => {
   return (
     <section style={{marginBottom: '50vh'}} className='flow'>
         <h2 className='numbered-title'><span>03</span>Interactive Elements</h2>
+        
+        {/* Navigation */}
+        <div>
+          <nav>
+            <ul className="primary-navigation underline-indicators flex">
+              <li className='active'><a className="uppercase text-white letter-spacing-2" href="#"><span>01</span>Active</a></li>
+              <li><a className="uppercase text-white letter-spacing-2" href="#"><span>02</span>Hovered</a></li>
+              <li><a className="uppercase text-white letter-spacing-2" href="#"><span>03</span>Idle</a></li>
+            </ul>
+          </nav>
+        </div>
+
+        {/* Explore Button */}
         <div style={{marginTop: '5rem'}} className='flex'>
             <a className='uppercase ff-serif text-dark bg-white large-button fs-600 ' href="#">Explore</a>
         </div>
--- src/styles/ComponentStyles.ts
+++ src/styles/ComponentStyles.ts
@@ -1,35 +1,80 @@
-import { css } from 'styled-components';
+import { css } from "styled-components";
 
 const ComponentStyles = css`
+  .large-button {
+    position: relative;
+    z-index: 1;
+    border-radius: 50%;
+    padding: 0 2em;
+    display: grid;
+    aspect-ratio: 1;
+    text-decoration: none;
+    place-items: center;
 
-    .large-button {
-        position: relative;
-        z-index: 1;
-        border-radius: 50%;
-        padding: 0 2em;
-        display: grid;
-        aspect-ratio: 1;
-        text-decoration: none;
-        place-items: center;
-
-        ::after {
-            content: '';
-            z-index: -1;
-            position: absolute;
-            background: hsl(var(--clr-white) /.15);
-            width: 100%;
-            height: 100%;
-            border-radius: 50%;
-            transition: opacity 500ms linear, transform 750ms ease-in-out;
-            opacity: 0;
-        }
-
-        :hover::after,
-        :focus::after {
-            transform: scale(1.5);
-            opacity: 1;
-        }
+    ::after {
+      content: "";
+      z-index: -1;
+      position: absolute;
+      background: hsl(var(--clr-white) / 0.15);
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      transition: opacity 500ms linear, transform 750ms ease-in-out;
+      opacity: 0;
     }
+
+    :hover::after,
+    :focus::after {
+      transform: scale(1.5);
+      opacity: 1;
+    }
+  }
+
+  .primary-navigation {
+    --gap: 8rem;
+    --underline-gap: 2rem;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+
+    a {
+      text-decoration: none;
+
+      span {
+        font-weight: 700;
+        margin-right: 0.5rem;
+      }
+    }
+  }
+
+  .underline-indicators {
+    > * {
+      position: relative;
+      padding: var(--underline-gap, 1rem) 0;
+
+      ::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        height: 0.1rem;
+      }
+
+      :hover,
+      :focus {
+        ::after {
+          background-color: hsl(var(--clr-white) / .5);
+        }
+      }
+
+      &.active {
+        ::after {
+          background-color: hsl(var(--clr-white));
+        }
+      }
+    }
+  }
 `;
 
-export default ComponentStyles
+export default ComponentStyles;
