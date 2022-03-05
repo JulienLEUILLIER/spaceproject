09d5bfda4c7a1312671c1ca551a0fd225330095d
JulienLEUILLIER <julien.leuillier@gmail.com>
2022-03-05 22:28:34 +0100

    Changed configuration to not use CRA

---
+++ webpack/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+          type: 'asset/inline',
+        },
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, '..', './build'),
+      filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, '..', './src/index.html')
+        })
+    ],
+    devServer: {
+      hot: true
+    },
+    stats: 'errors-only',
+  }
