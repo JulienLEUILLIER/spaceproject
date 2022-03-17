9dc9cbf9337f226425536ca055c46942f5c73863
JulienLEUILLIER <julien.leuillier@gmail.com>
2022-03-17 18:06:18 +0100

    Revert "Trying React-scripts"
    This reverts commit 3555d08eef95d0340eac852b3063551659119e31.

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom'
 import { NavigatorWrapper } from './Header.styles';
 import { MainSection } from './Header';
 import ListItem from './ListItem';
